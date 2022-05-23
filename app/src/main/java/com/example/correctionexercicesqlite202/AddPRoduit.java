213eb0991c44e7dfa7aa4e7169b6dbe9b7e86cb2
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-23 17:42:14 +0100

    Ajouter produit

--- app/src/main/java/com/example/correctionexercicesqlite202/AddPRoduit.java
+++ app/src/main/java/com/example/correctionexercicesqlite202/AddPRoduit.java
@@ -1,14 +1,45 @@
 package com.example.correctionexercicesqlite202;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AddPRoduit extends AppCompatActivity {
+
+    EditText elib, eprixa, eprixv;
+    Spinner efam;
+    MyDBProduit db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_produit);
+
+        db = new MyDBProduit(this);
+
+        elib = findViewById(R.id.elib);
+        efam = findViewById(R.id.efam);
+        eprixa = findViewById(R.id.eprixa);
+        eprixv = findViewById(R.id.eprixv);
+    }
+
+    public void ajouter(View view) {
+
+        Produit p = new Produit();
+
+        p.setLibelle(elib.getText().toString());
+        p.setFamille(efam.getSelectedItem().toString());
+        p.setPrixAchat(Double.parseDouble(eprixa.getText().toString()));
+        p.setPrixVente(Double.parseDouble(eprixv.getText().toString()));
+
+        if (MyDBProduit.insert_produit(db.getWritableDatabase(), p) == -1)
+            Toast.makeText(this, "Insertion Echoue", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Insertion reussie", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/correctionexercicesqlite202/MainActivity.java
+++ app/src/main/java/com/example/correctionexercicesqlite202/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.correctionexercicesqlite202;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void acces(View view) {
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.b1: i = new Intent(MainActivity.this,ListerProduit.class); break;
+            case R.id.b2: i = new Intent(MainActivity.this,FicheProduit.class); break;
+            case R.id.b3: i = new Intent(MainActivity.this,AddPRoduit.class); break;
+            case R.id.b4: i = new Intent(MainActivity.this,EditProduit.class); break;
+        }
+        startActivity(i);
     }
 }
